6cbfa5a349671ceb2ed272f09e3f9a9b23751c1d
Sameh Zaguia <samehzaguia@ieee.org>
2020-08-29 20:52:08 +0100

    Updated

--- app/src/main/java/com/example/sfmapp/SettingsActivity.java
+++ app/src/main/java/com/example/sfmapp/SettingsActivity.java
@@ -7,18 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class SettingsActivity extends AppCompatActivity {
-Button ac,ac_list;
+Button ac,ac_list,signout;
+FloatingActionButton save;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
-        //navbar buttons
+        //assign views
         ac=findViewById(R.id.ac);
         ac_list=findViewById(R.id.ac_list);
+        save=findViewById(R.id.confirm);
 
+        //navbar buttons
         ac_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
